e937839ad1e0df609df20135fa53ff87f5937f4f
devanshi2123 <95473477+devanshi2123@users.noreply.github.com>
2022-10-15 10:07:13 +0530

    Add files via upload

---
+++ assgiment_1.py
@@ -0,0 +1,93 @@
+
+
+#2
+# j=[1,2,3,4,5,6]
+# even=[]
+# odd =[]
+# for i in j:
+#     if(i%2==0):
+#         even.append(i)
+#     else:
+#         odd.append(i)
+# print("Even List:",even)
+# print ("Odd list",odd)
+
+#3
+# def lower(str):
+#     return str.lower()
+# str=map(lower,("HI","GOOD MORNING","I AM DEVANSHI"))
+# print(tuple(str))
+
+#4
+# def root(i):
+#     #p=power
+#     p=0.5
+#     return i**p
+# i=map(root,[2,10,20])
+# print(list(i))
+
+#5
+# def removeduplicate(str, n):
+#     s = set()
+#     for i in str:
+#         s.add(i)
+#     st =""
+#     for i in s:
+#         st = st+i
+#     return st
+# str = "HELLLOOO DEVANSHI SHAH"
+# n = len(str)
+# print(removeduplicate(list(str), n))
+
+
+
+# #6
+# n = int(input("Enter the number: "))
+# for i in range (1, 20):
+#     print(n, "x", i, "=", n * i)
+
+
+#7
+# i=[1,2,3]
+# j=[4,5,6]
+# print(list(i+j))
+
+#8
+# num1 = 1030.8
+# num2 = 293.999999
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+# print(num1, num2, sep = '\n')
+
+#9
+
+# seqtuple = ('A', 'B', 'C', 'D', 'E')
+# print(list(reversed(seqtuple)))
+
+# seqrange = range(1, 5)
+# print(list(reversed(seqrange)))
+
+#10
+devBio = {
+  "name": "DEVANSHI",
+  "name": "khushi",
+  "name": "rajvi",
+  "age": 12,
+  "language": "Script"
+}
+print(devBio)
+# {'name': 'Goli', 'age': 12, 'language': 'Script'}
+
+
+
+
+
+
+
+
+
+
+
+
